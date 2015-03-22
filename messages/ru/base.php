88cbb007d55ba3b7b8c3e30884a26acb4b60dd83
Sam Sheridan <programguy@users.noreply.github.com>
2015-03-22 13:31:36 -0400

    Fix typo

--- messages/ru/base.php
+++ messages/ru/base.php
@@ -5,7 +5,7 @@ return array (
   'Most Active Users Module Configuration' => 'Настройки модуля Самые активные пользователи',
   'No users.' => 'Нет пользователей.',
   'Save' => 'Сохранить',
-  'The number of most actice users that will be shown.' => 'Количество активных пользователей, которое будет отображаться.',
+  'The number of most active users that will be shown.' => 'Количество активных пользователей, которое будет отображаться.',
   'The number of users must not be greater than a 7.' => 'Количество пользователей не должно быть больше 7.',
   'The number of users must not be negative.' => 'Количество пользователей не должно быть отрицательным.',
   'You may configure the number users to be shown.' => 'Вы можете настроить количество пользователей для отображения.',
