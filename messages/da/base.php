364e9ddd8490640eb678f089fc9c7e39eb802777
Lucas Bartholemy <lucas@bartholemy.com>
2015-05-27 14:56:50 +0200

    Updated translations (translate.humhub.org)

--- messages/da/base.php
+++ messages/da/base.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Most</strong> active people' => '',
-  'Back to modules' => '',
-  'Get a list' => '',
-  'Most Active Users Module Configuration' => '',
-  'No users.' => '',
+  '<strong>Most</strong> active people' => '<strong>Mest</strong> aktive personer',
+  'Back to modules' => 'Tilbage til moduler',
+  'Get a list' => 'Få en liste',
+  'Most Active Users Module Configuration' => 'Mest Aktive Brugere Modul konfiguration',
+  'No users.' => 'Ingen brugere',
   'Save' => 'Gem',
-  'The number of most actice users that will be shown.' => '',
-  'The number of users must not be greater than a 7.' => '',
-  'The number of users must not be negative.' => '',
-  'You may configure the number users to be shown.' => '',
+  'The number of most actice users that will be shown.' => 'Antallet af mest aktive brugere som vil blive vist.',
+  'The number of users must not be greater than a 7.' => 'Antallen af brugere må ikke være større end 7.',
+  'The number of users must not be negative.' => 'Antallet af brugere må ikke være negativt.',
+  'You may configure the number users to be shown.' => 'Du må gerne konfigurere antal brugere som skal vises.',
 );
