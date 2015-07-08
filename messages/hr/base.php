6ee4c6f30c88df81893d9ac06839a1614e111584
Lucas Bartholemy <lucas@bartholemy.com>
2015-07-08 22:37:54 +0200

    Added Croatian language files

---
+++ messages/hr/base.php
@@ -0,0 +1,13 @@
+<?php
+return array (
+  '<strong>Most</strong> active people' => '',
+  'Back to modules' => '',
+  'Get a list' => '',
+  'Most Active Users Module Configuration' => '',
+  'No users.' => '',
+  'Save' => '',
+  'The number of most actice users that will be shown.' => '',
+  'The number of users must not be greater than a 7.' => '',
+  'The number of users must not be negative.' => '',
+  'You may configure the number users to be shown.' => '',
+);
