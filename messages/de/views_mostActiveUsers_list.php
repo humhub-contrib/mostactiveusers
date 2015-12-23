0ef1e0e759ac1fe2b8908f1027ac5087248fde7f
HumHub Translate <info@humhub.com>
2015-12-23 22:16:29 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/views_mostActiveUsers_list.php
+++ messages/de/views_mostActiveUsers_list.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  '<strong>Most</strong> active people' => '',
+  '<strong>Most</strong> active people' => 'Die <strong>aktivsten</strong> Benutzer',
   'Comments created' => 'Erstellte Kommentare',
   'Likes given' => 'Gegebene Likes',
   'Posts created' => 'Erstellte Posts',
