5d292b1af3f89791dd97483e77728470adbf294a
HumHub Translations <info@humhub.com>
2018-03-25 21:04:41 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/base.php
+++ messages/hu/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>Most</strong> active people' => '',
-  'Back to modules' => '',
-  'Get a list' => '',
-  'Most Active Users Module Configuration' => '',
+  '<strong>Most</strong> active people' => '<strong>Legaktivabb</strong> emberek',
+  'Back to modules' => 'Vissza a modulokhoz',
+  'Get a list' => 'Lista mutatása',
+  'Most Active Users Module Configuration' => 'Legaktívabb Felhasználók modul beállítása',
   'Save' => 'Mentés',
-  'The number of most active users that will be shown.' => '',
-  'You may configure the number users to be shown.' => '',
+  'The number of most active users that will be shown.' => 'Legaktívabb felhasználók száma.',
+  'You may configure the number users to be shown.' => 'Beállíthatod a megjelenő felhasználók számát.',
 );
