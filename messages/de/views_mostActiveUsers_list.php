485547bfdd555eca00d6f99da4d99fcc01faa635
HumHub Translations <info@humhub.com>
2020-02-27 11:50:06 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/de/views_mostActiveUsers_list.php
+++ messages/de/views_mostActiveUsers_list.php
@@ -3,5 +3,5 @@ return array (
   '<strong>Most</strong> active people' => '<strong>Aktivste</strong> Benutzer',
   'Comments created' => 'Erstellte Kommentare',
   'Likes given' => 'Abgegebene "Gefällt mir"',
-  'Posts created' => 'Erstellte Posts',
+  'Posts created' => 'Erstellte Beiträge',
 );
