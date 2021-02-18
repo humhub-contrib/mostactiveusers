eeb2e002e92c6bd7777c5cae1f47c614a0ec6405
HumHub Translations <info@humhub.com>
2021-02-18 10:29:43 +0100

    Enh: Updated Translations (translate.humhub.org)

--- messages/hu/views_mostActiveUsers_list.php
+++ messages/hu/views_mostActiveUsers_list.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   '<strong>Most</strong> active people' => '<strong>Legaktivabb</strong> emberek',
-  'Comments created' => 'Hozzászólások',
-  'Likes given' => 'Kedvelések',
-  'Posts created' => 'Létrehozott bejegyzések',
+  'Comments created' => 'Hozzászólások létrehozva',
+  'Likes given' => 'Adott kedvelések',
+  'Posts created' => 'Bejegyzések létrehozva',
 );
