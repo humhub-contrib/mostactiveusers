0c8ad7fd23b5bbc5ea0c172806421e14258b3631
HumHub Translations <info@humhub.com>
2021-06-10 20:14:10 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/th/base.php
+++ messages/th/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>Most</strong> active people' => '',
-  'Back to modules' => '',
-  'Get a list' => '',
-  'Most Active Users Module Configuration' => '',
+  '<strong>Most</strong> active people' => '<strong>ส่วนใหญ่</strong> คนที่กระตือรือร้น active',
+  'Back to modules' => 'กลับไปที่โมดูล',
+  'Get a list' => 'รับรายการ',
+  'Most Active Users Module Configuration' => 'การกำหนดค่าโมดูลผู้ใช้ที่ใช้งานมากที่สุด',
   'Save' => 'บันทึก',
-  'The number of most active users that will be shown.' => '',
-  'You may configure the number users to be shown.' => '',
+  'The number of most active users that will be shown.' => 'จำนวนผู้ใช้ที่ใช้งานมากที่สุดที่จะแสดง',
+  'You may configure the number users to be shown.' => 'คุณสามารถกำหนดค่าจำนวนผู้ใช้ที่จะแสดง',
 );
