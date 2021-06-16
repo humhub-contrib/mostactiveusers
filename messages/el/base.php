ffa9ae09a29e5e53354770d89a9e1d67e9e03441
HumHub Translations <info@humhub.com>
2021-06-16 13:37:15 +0200

    Enh: Updated Translations (translate.humhub.org)

--- messages/el/base.php
+++ messages/el/base.php
@@ -5,6 +5,6 @@ return array (
   'Get a list' => '',
   'Most Active Users Module Configuration' => '',
   'Save' => 'Αποθήκευση',
-  'The number of most active users that will be shown.' => '',
+  'The number of most active users that will be shown.' => 'Ο μέγιστος αριθμός ενεργών χρηστών που θα φαίνεται.',
   'You may configure the number users to be shown.' => '',
 );
