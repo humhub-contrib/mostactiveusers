1b0ed82b2b898c1d3fdfec4eb9b9bc74243debea
HumHub Translations <hello@humhub.com>
2022-09-06 16:32:49 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/pt/base.php
+++ messages/pt/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>Most</strong> active people' => '',
+  '<strong>Most</strong> active people' => 'Utilizadores <strong>mais ativos</strong>',
   'Back to modules' => 'Voltar para os módulos',
-  'Get a list' => '',
-  'Most Active Users Module Configuration' => '',
+  'Get a list' => 'Obter uma lista',
+  'Most Active Users Module Configuration' => 'Configuração do Módulo Utilizadores Mais Ativos',
   'Save' => 'Guardar',
   'The number of most active users that will be shown.' => 'O número de pessoas mais activas que serão mostradas.',
-  'You may configure the number users to be shown.' => '',
+  'You may configure the number users to be shown.' => 'Pode configurar o número de utilizadores a exibir.',
 );
