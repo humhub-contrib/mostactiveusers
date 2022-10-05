5d38c2d150b552c0336a368bc2156b03567df1aa
HumHub Translations <hello@humhub.com>
2022-10-05 12:21:14 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/nb-NO/base.php
+++ messages/nb-NO/base.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  '<strong>Most</strong> active people' => '<strong>Flest</strong> aktive brukere',
+  '<strong>Most</strong> active people' => '<strong>Mest</strong> aktive brukere',
   'Back to modules' => 'Tilbake til moduler',
   'Get a list' => 'Se listen',
-  'Most Active Users Module Configuration' => 'Flest aktive brukere modul konfigurasjon',
+  'Most Active Users Module Configuration' => 'Mest aktive brukere modulkonfigurasjon',
   'Save' => 'Lagre',
-  'The number of most active users that will be shown.' => 'Det høyeste nummer av aktive brukere som vil vises',
+  'The number of most active users that will be shown.' => 'Antall mest aktive brukere som vil vises',
   'You may configure the number users to be shown.' => 'Du kan konfigurere antall brukere som vises',
 );
