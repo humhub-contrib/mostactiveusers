de381dba4f8f4b3c9face4583094c5a716ec4710
HumHub Translations <hello@humhub.com>
2023-01-10 18:29:01 +0000

    Enh: Updated Translations (translate.humhub.org)

---
+++ messages/et/base.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    '<strong>Most</strong> active people' => '',
+    'Back to modules' => '',
+    'Get a list' => '',
+    'Most Active Users Module Configuration' => '',
+    'Save' => '',
+    'The number of most active users that will be shown.' => '',
+    'You may configure the number users to be shown.' => '',
+];
