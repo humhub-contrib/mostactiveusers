39de640ec7ee5f5ce49c07e2c85a6680bbd1bf90
HumHub Translations <hello@humhub.com>
2023-04-28 18:26:21 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/ja/views_mostActiveUsers_list.php
+++ messages/ja/views_mostActiveUsers_list.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   '<strong>Most</strong> active people' => '最もアクティブな人々',
-  'Comments created' => '作成されたコメント',
-  'Likes given' => '与えられた好き',
-  'Posts created' => '作成された投稿',
+  'Comments created' => 'コメント数',
+  'Likes given' => 'いいね！の数',
+  'Posts created' => '投稿数',
 );
