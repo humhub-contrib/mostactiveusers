7b50e2a9c0ceacffeff69795d6969a9bae18a486
HumHub Translations <hello@humhub.com>
2025-09-10 17:09:43 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/sl/base.php
+++ messages/sl/base.php
@@ -7,7 +7,7 @@ return [
   'Likes given' => '',
   'Most Active Users Module Configuration' => '',
   'Posts created' => '',
-  'Save' => '',
+  'Save' => 'Shrani',
   'The number of most active users that will be shown.' => '',
   'You may configure the number users to be shown.' => '',
 ];
